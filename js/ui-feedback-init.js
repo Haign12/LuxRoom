e4f5f950cf4d315aa41d79bb76d3f19ec8c2825c
Haign12 <9.3433772e+07+Haign12@users.noreply.github.com>
2026-08-20 12:20:19 +0000

    feat: add UI Feedback update button

--- js/ui-feedback-init.js
+++ js/ui-feedback-init.js
@@ -1,4 +1,4 @@
-import { createUIFeedback } from '../ui-feedback.js?v=ui-feedback-v0.5.1-drag-fix';
+import { createUIFeedback } from '../ui-feedback.js?v=ui-feedback-v0.6.0-update-button';
 
 createUIFeedback({
   storageKey: 'luxroom-ui-feedback',
