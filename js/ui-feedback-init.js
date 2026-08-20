65f3bf725ec97be8a52389d799b86da957d7ad0e
Haign12 <9.3433772e+07+Haign12@users.noreply.github.com>
2026-08-20 12:45:50 +0000

    feat: sync UI Feedback visual refresh

--- js/ui-feedback-init.js
+++ js/ui-feedback-init.js
@@ -1,4 +1,4 @@
-import { createUIFeedback } from '../ui-feedback.js?v=ui-feedback-v0.6.1-update-mirror';
+import { createUIFeedback } from '../ui-feedback.js?v=ui-feedback-v0.7.0-visual-refresh';
 
 createUIFeedback({
   storageKey: 'luxroom-ui-feedback',
