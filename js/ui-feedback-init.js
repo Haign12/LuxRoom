0eee291dbe4e8c5aa4d54bee9c1873971f172091
Nghĩaaaa <93433772+Haign12@users.noreply.github.com>
2026-08-23 13:32:23 +0700

    chore: update UI Feedback tool to v0.14.0

--- js/ui-feedback-init.js
+++ js/ui-feedback-init.js
@@ -1,4 +1,4 @@
-import { createUIFeedback } from '../ui-feedback.js?v=0.13.0';
+import { createUIFeedback } from '../ui-feedback.js?v=0.14.0';
 
 createUIFeedback({
   storageKey: 'luxroom-ui-feedback',
