9a6809453eab3610c9ebcf3dae04c76e7ef3e95a
Haign12 <9.3433772e+07+Haign12@users.noreply.github.com>
2026-08-23 07:20:42 +0000

    chore: sync UI Feedback Tool e6127a1

--- js/ui-feedback-init.js
+++ js/ui-feedback-init.js
@@ -1,4 +1,4 @@
-import { createUIFeedback } from '../ui-feedback.js?v=0.14.0';
+import { createUIFeedback } from '../ui-feedback.js?v=e6127a1';
 
 createUIFeedback({
   storageKey: 'luxroom-ui-feedback',
